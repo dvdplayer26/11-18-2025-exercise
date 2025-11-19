72802546af7c85f2a6648aad35106bd5cf6330b2
dvdplayer26 <davidl526@nycstudents.net>
2025-11-19 09:24:00 -0500

    Finished code

--- sumofpositives.py
+++ sumofpositives.py
@@ -1,25 +1,48 @@
+# First step: define all your values and make your initial list into a global to be used in functions.
+global my_list
+my_list = []
+
+global pos_sum
+pos_sum = 0
+
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+
+# Second step: create def
 def sum_of_positives(my_list):
+
+    # Access global list
+    global pos_sum
+
+    # Modify variables and display
     a = float(input("Please enter first number: "))
     b = float(input("Please enter second number: "))
     c = float(input("Please enter third number: "))
     d = float(input("Please enter fourth number: "))
     e = float(input("Please enter fifth number: "))
     my_list = [a,b,c,d,e]
+    print("List of numbers: ",my_list)
+
+    # Separate positives, display  and add them together.
     pos_list = []
-    if a > 0
+    if a > 0:
         pos_list.append(a)
-    if b > 0
+    if b > 0:
         pos_list.append(b)
-    if c > 0
+    if c > 0:
         pos_list.append(c)
-    if d > 0
+    if d > 0:
         pos_list.append(d)
-    if e > 0
+    if e > 0:
         pos_list.append(e)
-    sum = pos_list.index(0) + pos_list.index(1) + pos_list.index(2) + pos_list.index(3) + pos_list.index(4)
+    print("List of positive values: ",pos_list)
+    pos_sum += sum(pos_list)
     
-        
-    print("The result is ", sum)
+    # Print sum
+    print("The result is ", pos_sum)
 
+# Third step: run function
 sum_of_positives(my_list)
-
