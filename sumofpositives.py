e6f3f0f195e7e571ad08883f403f032d0a57123e
dvdplayer26 <davidl526@nycstudents.net>
2025-11-19 08:55:03 -0500

    Make most of the code
    Refactored the function to sum positive numbers and changed input handling.

--- sumofpositives.py
+++ sumofpositives.py
@@ -1,7 +1,25 @@
-def SummationMake():
-    my_list = [int(input("Please enter first number: ")),int(input("Please enter second number: ")),int(input("Please enter third number: ")),int(input("Please enter fourth number: ")),int(input("Please enter fifth number: "))]
-    results = sum_of_positives(my_list)
-    print("The result is ", results)
+def sum_of_positives(my_list):
+    a = float(input("Please enter first number: "))
+    b = float(input("Please enter second number: "))
+    c = float(input("Please enter third number: "))
+    d = float(input("Please enter fourth number: "))
+    e = float(input("Please enter fifth number: "))
+    my_list = [a,b,c,d,e]
+    pos_list = []
+    if a > 0
+        pos_list.append(a)
+    if b > 0
+        pos_list.append(b)
+    if c > 0
+        pos_list.append(c)
+    if d > 0
+        pos_list.append(d)
+    if e > 0
+        pos_list.append(e)
+    sum = pos_list.index(0) + pos_list.index(1) + pos_list.index(2) + pos_list.index(3) + pos_list.index(4)
+    
+        
+    print("The result is ", sum)
 
-SummationMake()
+sum_of_positives(my_list)
 
